7cb5b36beecde76adad053f3fbdeb960c55927a4
unknown <denizyesilirmak@gmail.com>
2020-08-23 23:27:38 +0300

    added carousel to intro component

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { View } from 'react-native'
 import CityPicker from './Components/Cities/CityPicker'
-
+import Intro from './Components/Intro/Intro'
 
 class App extends React.Component {
   render(){
     return(
       <View>
+        <Intro/>
       </View>
     )
   }
--- Components/Intro/Intro.js
+++ Components/Intro/Intro.js
@@ -1,12 +1,65 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text, StyleSheet, Dimensions, Image } from 'react-native'
+import Carousel from 'react-native-snap-carousel';
 
-class Intro extends React.Component{
-  render(){
-    return(
+import A from '../../Assets/intro/1.jpg'
+import B from '../../Assets/intro/2.jpg'
+import C from '../../Assets/intro/3.jpg'
+import D from '../../Assets/intro/4.jpg'
+
+const entries = [
+  {
+    image: A
+  },
+  {
+    image: B
+  },
+  {
+    image: C
+  },
+  {
+    image: D
+  },
+]
+
+class Intro extends React.Component {
+
+  _renderItem = ({ item, index }) => {
+    return (
+      <View style={styles.slide}>
+        <Image source={item.image} style={{ width:'100%', height: 600, resizeMode: 'cover' }} />
+      </View>
+    );
+  }
+
+  render() {
+    return (
       <View>
-        
+        <Carousel
+          ref={(c) => { this._carousel = c; }}
+          data={entries}
+          renderItem={this._renderItem}
+          sliderWidth={Dimensions.get('screen').width}
+          itemWidth={Dimensions.get('screen').width}
+          s
+        />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  slide: {
+    width: Dimensions.get('window').width - 50,
+    height: 600,
+    elevation: 2,
+    marginBottom: 10,
+    marginLeft: 25,
+    borderRadius: 20,
+    overflow: 'hidden',
+    marginTop: 20
+
+  }
+})
+
+export default Intro
